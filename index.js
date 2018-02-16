d0209ca4cd22b196846a0712dba862bf15e032d6
cmilkaitis <chris.milkaitis@gmail.com>
2018-02-16 18:15:04 -0500

    refactored

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 const jediName = function(firstName, lastName) {
-  console.log(lastName.slice(0, 3) + firstName.slice(0, 2));
+  return(lastName.slice(0, 3) + firstName.slice(0, 2));
 };
 
-jediName('Beyonce', 'Knowles');
+console.log(jediName('Beyonce', 'Knowles'));
